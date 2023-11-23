f464eb8fc53c54fdf288a2bedb3e2284c96b8414
Karol osorio <kd.osorio27@ciaf.edu.co>
2023-11-22 23:21:11 -0500

    testingkarol

--- src/suma/suma.js
+++ src/suma/suma.js
@@ -4,13 +4,3 @@ function suma(a, b) {
 }
 
 module.exports = suma;
-// unidad de prueba
-// ¿ Qué hace la función dos numeros ? la suma de dos numeros
-function sum(num1, num2) {
-    if (typeof num1 != "number" || typeof num2 != "number") {
-      return "datos invalidos";
-    }
-    return num1 + num2;
-  }
-  
-  module.exports = 
